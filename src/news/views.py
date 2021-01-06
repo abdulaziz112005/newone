32d168c3c6982f3ae9e856dc7d2c834e1a863a83
jurayevqobiljon@gmail.com <q8280234d>
2021-01-06 14:39:40 +0300

    models and views changed

--- src/news/views.py
+++ src/news/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Tags, Articles
 
 
 def index(request):
     return render(request, 'main/index.html')
 
 def category(request):
-    categories = Category.objects.all
+    categories = Category.objects.all()
+    tags = Tags.objects.all()
+    articles = Articles.objects.all()
     context = {
-        'categories': categories
+        'categories': categories,
+        'tags': tags,
+        'articles': articles
     }
+    print(context)
     return render(request, 'main/category.html', context)
