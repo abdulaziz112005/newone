84d3905ed4e5becbb0577777d7a3c6875cff76b9
aziz785 <mubashirov2002@gmail.com>
2021-01-07 17:32:19 +0500

    contact app dagi views.py  tayyor

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,12 +1,13 @@
 from .forms import NameForm
 from django.shortcuts import render, get_object_or_404, reverse
 from .models import *
+from news.models import Category
 from django.http import HttpResponseRedirect
 
 
 def contact(request):
     contacts = Contact.objects.get(pk=1)
-
+    categorys = Category.objects.all()
 
     if request.method == 'POST':
         form = NameForm(request.POST)
@@ -15,7 +16,7 @@ def contact(request):
     else:
         form = NameForm()
 
-    context = {'contacts': contacts, 'form': form }
+    context = {'contacts': contacts, 'form': form, 'categorys':categorys }
     return render(request, 'main/name.html' , context)
 
 
