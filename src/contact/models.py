2681b66abf9c2d7d1801da3cbf11295800882641
aziz785 <mubashirov2002@gmail.com>
2021-01-07 18:34:33 +0500

    forms.ModelForm tayyor

--- src/contact/models.py
+++ src/contact/models.py
@@ -19,3 +19,14 @@ class Contact(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(unidecode(self.name))
         super(Contact, self).save(*args, **kwargs)
+
+
+class Feedback(models.Model):
+    fullname = models.CharField(max_length=40)
+    email = models.EmailField(max_length=100, null=True)
+    phone_number = models.CharField(max_length=13)
+    message = models.TextField()
+    subject = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.fullname
--- src/contact/views.py
+++ src/contact/views.py
@@ -1,4 +1,4 @@
-from .forms import NameForm
+from .forms import ProductForm
 from django.shortcuts import render, get_object_or_404, reverse
 from .models import *
 from news.models import Category
@@ -9,12 +9,9 @@ def contact(request):
     contacts = Contact.objects.get(pk=1)
     categorys = Category.objects.all()
 
-    if request.method == 'POST':
-        form = NameForm(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect(reverse('news:form'))
-    else:
-        form = NameForm()
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
 
     context = {'contacts': contacts, 'form': form, 'categorys':categorys }
     return render(request, 'main/name.html' , context)
