bdfe5efca9d606097376a7a9754836b51e10dfee
coool <forcorp9@gmail.com>
2021-01-08 19:54:20 +0500

    temp

--- src/news/templatetags/menu.py
+++ src/news/templatetags/menu.py
@@ -1,7 +1,10 @@
 from django import template
-
+from ..models import Category
 register = template.Library()
 
 @register.inclusion_tag('tags/menu.html')
 def show_menu():
-    pass
+    categories = Category.objects.all()
+    return {
+        'categories': categories
+    }
